19b2d3c43c325df9df3cef3024ce600bf5a2496a
BruhatM <bruhatm@outlook.com>
2021-03-13 12:31:02 -0800

    scaler complete

--- R/scaler.R
+++ R/scaler.R
@@ -1,20 +1,66 @@
 #' Scaling a dataframe
 #'
-#' This function scales numerical features based on scaling requirement in a data.frame
+#' This function scales numerical features based on scaling requirement(standardization, minmax Scaling) in a data.frame
 #'
 #' @param X_train data.frame
-#' @param X_Valid data.frame
+#' @param X_valid data.frame
 #' @param X_test data.frame
-#' @param scale_features character vector
-#' @param scaler_type character 
+#' @param scaler_type character
 #'
 #' @return data.frame of data.frames
 #' @export
 #'
 #' @examples
-#' X_train<- data.frame('name' = c('pandaman', 'doorman', 'eve'), 'age' = c(15,20,25), 'networth' = c(100000,100,100000))
-#' X_Valid<- data.frame('name' = c('pandaman_v', 'doorman_v', 'eve_v'), 'age' = c(15,56, 43) , 'networth' = c(123124, 352334,645645)
-#' X_test <- data.frame('name' = c('pandaman_t', 'doorman_t', 'eve_t'), 'age' =c(14,15,56), 'networth' = c(123124,90914, 124124)
-#' scaled_df <- scaler(X_train, X_Valid, X_test, c('age','networth'), scaler_type='standardscaler')
-scaler <- function(X_train, X_Valid, X_test, scale_features, scaler_type){
+#' scaled_df <- scaler(X_train, X_Valid, X_test, scaler_type='standardization')
+scaler <- function(X_train, X_valid, X_test, scaler_type){
+
+  # checking if scaling methods are valid
+  if(!scaler_type %in% c('standardization', 'minmax')){
+    stop('Scaling method must be standardization or minmax')
+  }
+
+  #Checking input data type
+  if(!typeof(X_train) == 'list'){
+    stop('X_train should be a dataframe object')
+  }
+  if(!typeof(X_valid) == 'list'){
+    stop('X_valid should be a dataframe object')
+  }
+  if(!typeof(X_test) == 'list'){
+    stop('X_test should be a dataframe object')
+  }
+
+  #Checking if all data is numeric
+  if(!sum(sapply(X_train, is.numeric)) == dim(X_train)[2]){
+    stop('X_train must contain all numeric values')
+  }
+  if(!sum(sapply(X_valid, is.numeric)) == dim(X_valid)[2]){
+    stop('X_valid must contain all numeric values')
+  }
+  if(!sum(sapply(X_test, is.numeric)) == dim(X_test)[2]){
+    stop('X_test must contain all numeric values')
+  }
+
+
+  #Scaling Code
+  if (scaler_type == 'standardization'){
+    #Performing standardization
+    pp = caret::preProcess(X_train, method=c("center", "scale"))
+    X_train_scaled <- predict(pp, X_train)
+    X_valid_scaled <- predict(pp, X_valid)
+    X_test_scaled <- predict(pp, X_test)
+  }
+
+  if (scaler_type == 'minmax'){
+    #Performing Min-max Scaling
+    pp = caret::preProcess(X_train, method=c("range"))
+    X_train_scaled <- predict(pp, X_train)
+    X_valid_scaled <- predict(pp, X_valid)
+    X_test_scaled <- predict(pp, X_test)
+  }
+  out <- list()
+  out$train <- X_train_scaled
+  out$test <- X_test_scaled
+  out$valid <- X_valid_scaled
+  return(out)
 }
